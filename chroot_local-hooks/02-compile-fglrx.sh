d29cfb469bd025c307d60b71f8eab3a55c8b0b54
root <root@thelemasys.thelenet>
2008-10-19 09:33:07 +0200

    Updated to 2.6.26

--- chroot_local-hooks/02-compile-fglrx.sh
+++ chroot_local-hooks/02-compile-fglrx.sh
@@ -1,9 +1,9 @@
 which module-assistant || apt-get install --yes module-assistant
 
-module-assistant -l 2.6.24-1-486 --non-inter prepare
+module-assistant -l 2.6.26-1-486 --non-inter prepare
 
-module-assistant -l 2.6.24-1-486 --non-inter update
+module-assistant -l 2.6.26-1-486 --non-inter update
 
-module-assistant -l 2.6.24-1-486 --non-inter --quiet auto-install fglrx
+module-assistant -l 2.6.26-1-486 --non-inter --quiet auto-install fglrx
 
 module-assistant clean fglrx
